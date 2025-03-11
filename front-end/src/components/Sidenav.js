f5cafe053e984111c55a7a67bb04267cb3a51290
guan0612 <shig378@gmail.com>
2025-03-11 16:06:36 +0800

    modify Sidenav

--- front-end/src/components/Sidenav.js
+++ front-end/src/components/Sidenav.js
@@ -3,6 +3,13 @@ import '../style/Sidenav.css';
 import { Link } from 'react-router-dom';
 
 const Sidenav = () => {
+  const menuItems = [
+    { to: '/', icon: '📖', text: 'Introduction' },
+    { to: '/stock-graph', icon: '🕸️', text: 'Stock Relation Graph' },
+    { to: '/trading-strategy', icon: '💰', text: 'Trading Strategy' },
+    { to: '/settings', icon: '⚙️', text: 'Settings' },
+  ];
+
   return (
     <div className="sidenav">
       <div className="sidenav-header">
@@ -10,34 +17,18 @@ const Sidenav = () => {
       </div>
       <nav className="sidenav-menu">
         <ul>
-          <li>
-            <Link to="/">
-              <span className="icon">📖</span>
-              <span className="text">Introduction</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/stock-graph">
-              <span className="icon">🕸️</span>
-              <span className="text">Stock Relation Graph</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/trading-strategy">
-              <span className="icon">💰</span>
-              <span className="text">Trading Strategy</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/settings">
-              <span className="icon">⚙️</span>
-              <span className="text">Settings</span>
-            </Link>
-          </li>
+          {menuItems.map((item, index) => (
+            <li key={index}>
+              <Link to={item.to} aria-label={`Go to ${item.text}`}>
+                <span className="icon">{item.icon}</span>
+                <span className="text">{item.text}</span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
   );
 };
 
-export default Sidenav; 
+export default Sidenav;
